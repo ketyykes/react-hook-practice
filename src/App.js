9020f46fc0e48cab52931befc606f5cae38eb7e0
ketyykes <xi4869@gmail.com>
2022-05-16 01:32:28 +0800

    沒有使用hook就不能更新畫面

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from 'react'
+const App = () => {
+  let number = 123;
+  const incrementHandler = () => {
+    number +=1;
+    console.log(number);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{number}</div>
+      <button onClick={incrementHandler}>+</button>
     </div>
   );
 }
 
-export default App;
+export default App

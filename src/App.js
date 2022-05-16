a9b356277f4c7a93e40b7ae56c7cc2b94640ed55
ketyykes <xi4869@gmail.com>
2022-05-16 15:43:16 +0800

    使用setTimeout模擬非同步行為卻更新到同樣的值

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 const App = () => {
-  const [number,setNumber] = useState(123);
+  const [number, setNumber] = useState(123);
   const incrementHandler = () => {
-    setNumber(number + 1);
+    setTimeout(function delay() {
+      setNumber(number + 1);
+    }, 1000);
+
   };
   return (
     <div>

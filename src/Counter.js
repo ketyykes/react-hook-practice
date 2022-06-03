79d0d8286f1299b8dae0736ec9d7941b4a1ec1f4
ketyykes <xi4869@gmail.com>
2022-06-03 19:01:15 +0800

    將action拆出來並且再定義actionType

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,15 @@
 import React, { useReducer } from 'react';
 import reducer from './reducer';
 import initialState from './initialState'
+import { increment, decrement } from './actionCreate';
 function Counter() {
     const [state, dispatch] = useReducer(reducer, initialState);
+    console.log(increment(1));
     return (
         <>
             Count: {state.count}
-            <button onClick={() => dispatch({ type: 'decrement', payload: 1 })}>-</button>
-            <button onClick={() => dispatch({ type: 'increment', payload: 1 })}>+</button>
+            <button onClick={() => dispatch(increment(1))}>+</button>
+            <button onClick={() => dispatch(decrement(1))}>-</button>
         </>
     );
 }
---
+++ src/actionCreate.js
@@ -0,0 +1,13 @@
+import { INCREMENT, DECREMENT } from './actionType';
+export const increment = (number) => {
+    return {
+        type: INCREMENT,
+        payload: number
+    }
+}
+export const decrement = (number) => {
+    return {
+        type: DECREMENT,
+        payload: number
+    }
+}

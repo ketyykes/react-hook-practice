22afb37c651e71b1aee7dc518211f8034f9247b5
ketyykes <xi4869@gmail.com>
2022-07-06 15:59:57 +0800

    使用console得到useEffect生命週期

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 const App = () => {
-  let number = 123;
-  const incrementHandler = () => {
-    number +=1;
-    console.log(number);
-  };
+  console.log("我在AppFunction裡面");
+  const [number, setNumber] = useState(0);
+  const clickHandler = () => {
+    setNumber((prev) => (prev + 1));
+  }
+  useEffect(() => {
+    console.log("我在useEffect裡面");
+    return () => {
+      console.log("我在useEffect的callback裡面稱為clearup");
+    }
+  })
   return (
     <div>
-      <div>{number}</div>
-      <button onClick={incrementHandler}>+</button>
+      {number}
+      {console.log("我在render函式裡面")}
+      <button onClick={clickHandler}>+</button>
     </div>
   );
-}
 
-export default App
+}

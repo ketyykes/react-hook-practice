f9930f3d285e46ddfdef8b2479d20004f9b93eed
ketyykes <xi4869@gmail.com>
2022-07-07 19:56:31 +0800

    練習merage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,62 @@
-import React from "react";
-import App from "./App";
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
+import React, { useContext, createContext, useReducer } from "react";
+
+const numberContext = createContext();
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 }
+    case 'DECREMENT':
+      return { count: state.count - 1 }
+    default:
+      return state;
+  }
+}
+const incrementAction = {
+  type: 'INCREMENT'
+}
+const decrementAction = {
+  type: 'DECREMENT'
+}
+
+const Number = () => {
+  const { state } = useContext(numberContext);
+  console.log(state.count);
+  return (
+    <div>{state.count}</div>
+  )
+}
+
+const Button = () => {
+  const { dispatch } = useContext(numberContext);
+  return (
+    <div>
+      <button onClick={() => (dispatch(incrementAction))}>+</button>
+      <button onClick={() => (dispatch(decrementAction))} >-</button>
+    </div >
+  )
+}
+
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
+  return (
+    <numberContext.Provider value={{
+      state, dispatch
+    }}>
+      <Number />
+      <Button />
+    </numberContext.Provider>
+  )
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   //   <React.StrictMode>
-    <App />
+  <App />
   //   </React.StrictMode>
 );

a0e8a7929fdc76f4515846aa0bc226618bcd3033
ketyykes <xi4869@gmail.com>
2022-07-12 20:36:46 +0800

    memo帶入第二個參數

--- src/Todo.js
+++ src/Todo.js
@@ -3,10 +3,13 @@ const Todo = ({ listData, number }) => {
     console.log(number);
     return (
         <>
+            {number}
             {listData.map((element, index) => {
                 return (<div key={index}>{element.content}</div>)
             })}
         </>
     )
 }
-export default memo(Todo);
+export default memo(Todo, (prevProps, nextProps) => {
+    return prevProps.listData === nextProps.listData
+});

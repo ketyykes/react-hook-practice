25b55d7ab79215a9d04fa73f24ac28bf30a3cc76
danny <xi4869@gmail.com>
2024-03-06 18:56:04 +0800

    沒用memo 我被執行了

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { useState } from 'react'
-import Todo from './Todo';
+import React, { useState } from "react";
+import Todo from "./Todo";
 
 const App = () => {
-  const [number, setNumber] = useState(0);
-  const [listData, setListData] = useState([{
-    content: '要做的事情',
-    id: Date.now(),
-    done: false
-  }])
+	const [number, setNumber] = useState(0);
 
-  return (
-    <>
-      <div>{number}</div>
-      <button onClick={() => {
-        setNumber((prev) => (prev + 1));
-      }}>+
-      </button>
-      <Todo listData={listData} />
-    </>
-  )
-}
+	return (
+		<>
+			<div>{number}</div>
+			<button
+				onClick={() => {
+					setNumber((prev) => prev + 1);
+				}}
+			>
+				+
+			</button>
+			<Todo />
+		</>
+	);
+};
 
-export default App
+export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Todo = ({ listData }) => {
-    console.log('我被執行了');
-    return (
-        <>
-            {listData.map((element, index) => {
-                return (<div key={index}>{element.content}</div>)
-            })}
-        </>
-    )
-}
+const Todo = () => {
+	console.log("我被執行了");
+	const [listData, setListData] = useState([
+		{
+			content: "要做的事情",
+			id: Date.now(),
+			done: false,
+		},
+	]);
+	return (
+		<>
+			{listData.map((element, index) => {
+				return <div key={index}>{element.content}</div>;
+			})}
+		</>
+	);
+};
 
 export default Todo;

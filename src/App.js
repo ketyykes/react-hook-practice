5890040e42162bff6b127db736d65a495fdbb8d6
danny <xi4869@gmail.com>
2024-03-06 22:34:55 +0800

    沒用memo 我被執行了

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Todo from "./Todo";
 
 const App = () => {
 	const [number, setNumber] = useState(0);
-
+	const [value, setValue] = useState("");
+	const [name, setName] = useState("");
 	return (
 		<>
 			<div>{number}</div>
@@ -14,7 +15,15 @@ const App = () => {
 			>
 				+
 			</button>
-			<Todo />
+			<input value={value} onChange={(e) => setValue(e.target.value)} />
+			<button
+				onClick={() => {
+					setName(value);
+				}}
+			>
+				送出
+			</button>
+			<Todo name={name} />
 		</>
 	);
 };
--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Todo = () => {
+const Todo = ({ name }) => {
 	console.log("我被執行了");
-	const [listData, setListData] = useState([
+
+	const listData = [
 		{
 			content: "要做的事情",
 			id: Date.now(),
 			done: false,
 		},
-	]);
+	];
 	return (
 		<>
+			<div>{name}的代辦事項</div>
 			{listData.map((element, index) => {
 				return <div key={index}>{element.content}</div>;
 			})}
